7d1e4f59f11c6ed950e4171551cb560fe8056bee
Gabriel <gabriel@MacBook.local>
2022-11-15 21:32:12 -0800

    Funcao Sacar

--- main.go
+++ main.go
@@ -2,7 +2,56 @@ package main
 
 import "fmt"
 
+type DadosConta struct {
+	Titular string  `json:"Titular"`
+	Agencia int     `json:"Agência"`
+	Conta   int     `json:"Conta"`
+	Saldo   float64 `json:"Saldo"`
+}
+
+func (d *DadosConta) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= d.Saldo
+
+	if podeSacar {
+		resultado := d.Saldo - valorDoSaque
+		fmt.Printf("Saque realizado com sucesso no valor de R$%v Reais, Saldo restante R$ %v Reais\n", valorDoSaque, resultado)
+	} else {
+		fmt.Println("Saldo insuficiente.")
+	}
+	return ""
+}
+
 func main() {
 
-	fmt.Println("Meu banco")
+	PrimeiroCliente := DadosConta{"Guilherme Dias", 589, 123456, 12500}
+
+	SegundoCliente := DadosConta{
+		Titular: "Luciene Silva",
+		Agencia: 589,
+		Conta:   111233,
+		Saldo:   489.90,
+	}
+
+	TerceiroCliente := DadosConta{
+		Titular: "Diego Chaves",
+		Agencia: 589,
+		Conta:   543678,
+		Saldo:   9890.76,
+	}
+
+	QuartoCliente := DadosConta{"Bruna Silva", 289, 111223, 87672.90}
+
+	fmt.Println(PrimeiroCliente)
+	fmt.Println(SegundoCliente)
+	fmt.Println(TerceiroCliente)
+	fmt.Println(QuartoCliente)
+
+	contaDasilvia := DadosConta{}
+	contaDasilvia.Titular = "Silvia"
+	contaDasilvia.Saldo = 500.
+
+	contaDasilvia.Sacar(-600)
+	PrimeiroCliente.Sacar(200)
+	SegundoCliente.Sacar(500)
+
 }
